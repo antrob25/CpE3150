6712ab2a08983b05cd0ac178e4fbb939e5606231
Sithija Dasun Gunasinghe <sparks10.sg@gmail.com>
2020-04-01 18:59:36 -0500

    fixed bad reset and it going to the start of the code

--- CPEproject1/CPEproject1/main.asm
+++ CPEproject1/CPEproject1/main.asm
@@ -8,14 +8,12 @@
 
 LDI R16, 0
 LDI R17, 0xFF
-LDI R18, 0x1F
+LDI R18, 0x20
 LDI R19, 0
 SBI DDRE,4	;makes PE4 an output
 CBI PORTE,4	;clears bit PE4
-CBI DDRE,5	;makes PE5 an input
-SBI PORTE,5	;set bit PE5
-SBI DDRE,6	;makes PE6 an input
-SBI PORTE,6	;set bit PE6
+SBI DDRE,5	;makes PE5 an input
+CBI PORTE,5	;set bit PE5
 OUT DDRA, R16
 OUT PORTA, R17
 OUT DDRA,R16
@@ -26,51 +24,57 @@ OUT PORTC,R16	;PORTC = 0
 
 //INPUT
 RCALL BEEP //Beep to indicate when to press the starting button
-    LDI  R23, 2 //Delay for approximately 5 seconds
+/*    LDI  R23, 2 //Delay for approximately 5 seconds
     LDI  R24, 150
     LDI  R25, 216
     LDI  R26, 8
-L1: DEC  R26
-    BRNE L1
+L4: DEC  R26
+    BRNE L4
     DEC  R25
-    BRNE L1
+    BRNE L4
     DEC  R24
-	BRNE L1
+	BRNE L4
     DEC  R23
-    BRNE L1
-    NOP
+    BRNE L4
+    NOP*/
 	IN R19, PINA //input the number using PINA2-6
 	LSR R19 //Shift right to accurately show the numbers starting from the 0th bit
 	LSR R19
+	RCALL CLEAR
 
 START:	
 	CHECK_SW1:
 		SBIC PINA,0 // positive button
 		INC R19 	;increments R19 when positive button is pressed
 		CP R19, R18	
-		BRSH RESET	;calls Reset if R19 is greater than or equal to R18
-		CALL OUTPUT_DISPLAY	
+		BRSH RESET1	;calls Reset if R19 is greater than or equal to R18
+BACK1:	CALL OUTPUT_DISPLAY	
 		RCALL CLEAR	;calls clear to clear all relevant status registers
 		RJMP CHECK_SW2
 
 	CHECK_SW2:
 		SBIC PINA,1 //negative button
 		DEC R19		;decrements R19 when negative button is pressed
-		BRLT RESET	;calls Reset if R19 is less than 0
+		BRLT RESET2	;calls Reset if R19 is less than 0
 		RCALL CLEAR	;calls clear to clear all relevant status registers
-		CALL OUTPUT_DISPLAY	
+BACK2:	CALL OUTPUT_DISPLAY	
 		RJMP START	;jumps to the beginning of the input loop
 
-RESET:	CPSE R19, R18
-		LDI R19, 0x1F	;if R19 is not equal to R18 sets R19 to 0x1F --what is this for?
+RESET1:	
 		LDI R19, 0	;sets R19 to 0
-BEEP:	LDI R22, 100	;value to loop sound 100 times
+		RCALL BEEP
+		RJMP BACK1
+RESET2:
+		LDI R19, 0x1F
+		RCALL BEEP
+		RJMP BACK2
+BEEP:	LDI R22, 1	;value to loop sound 100 times
 	L2:	SBI PORTE, 4 // Sound loop ;sets PE4 to high--starts sound
-		RCALL DELAY 	
+		;RCALL DELAY 	
 		NOP
 		NOP
 		CBI PORTE, 4	;clears PE4 -- ends sound
-		RCALL DELAY
+		;RCALL DELAY
 		DEC R22		
 		BRNE L2		;Loops until R22 = 0 
 		RET
