4f11dce943b32ecefe6024c524058dbd8a54c5f0
Dan (wait for it) Napierkowski <Dan.Napierkowski@gmail.com>
2020-04-02 13:18:43 -0500

    Added buttons to increase and decrease sound time

--- CPEproject1/CPEproject1/main.asm
+++ CPEproject1/CPEproject1/main.asm
@@ -10,6 +10,8 @@ LDI R16, 0
 LDI R17, 0xFF
 LDI R18, 0x1F
 LDI R19, 0
+LDI R23, 0
+LDI R24, 0
 SBI DDRE,4	;makes PE4 an output
 CBI PORTE,4	;clears bit PE4
 CBI DDRA,0	;makes PA0 an input
@@ -24,6 +26,16 @@ OUT PORTC,R16	;PORTC = 0
 
 //INPUT
 START:	
+	CHECK_SW3:
+		SBIC PINA,2 //Make sound longer button
+		INC R23 ;Increments R23 when longer sound button is pressed
+		RJMP CHECK_SW4
+
+	CHECK_SW4:
+		SBIC PINA,3 //Make sound shorter button
+		DEC R23 ;Decrements R23 when shorter sound button is pressed
+		RJMP CHECK_SW1
+
 	CHECK_SW1:
 		SBIC PINA,0 // positive button
 		INC R19 	;increments R19 when positive button is pressed
@@ -41,6 +53,7 @@ BACK1:	OUT PORTD, R19
 BACK2:	OUT PORTD, R19
 		RJMP START	;jumps to the beginning of the input loop
 
+
 RESET1:	
 		LDI R19, 0	;sets R19 to 0
 		RCALL BEEP
@@ -49,7 +62,9 @@ RESET2:
 		LDI R19, 0x1F
 		RCALL BEEP
 		RJMP BACK2
-BEEP:	LDI R22, 100
+BEEP:
+		mov R24, R23 ;Set loop multiplier equal to longer and shorter sound register	
+	L4:	LDI R22, 100
 	L2:	SBI PORTE, 4 // Sound loop ;sets PE4 to high--starts sound
 		RCALL DELAY 	
 		NOP
@@ -58,6 +73,8 @@ BEEP:	LDI R22, 100
 		RCALL DELAY
 		DEC R22		
 		BRNE L2		;Loops until R22 = 0 
+		DEC R24
+		BRNE L4
 		RET
 
 CLEAR:	CLH	
